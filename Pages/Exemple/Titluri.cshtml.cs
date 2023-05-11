0d54e658b2ba53e144297eaa29816af59bd95053
Iurie Jurja <iuriejurja@hotmail.com>
2023-05-11 20:58:17 +0300

    Adăugare exemplu
    * Am adăugat exemplu cu titluri într-o pagină HTML.

---
+++ Pages/Exemple/Titluri.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebDesignRazor.Pages.Exemple
+{
+    public class TitluriModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
