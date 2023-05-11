f199a40f2bd15d5718d2df2e3593be9f3466ba3b
Iurie Jurja <iuriejurja@hotmail.com>
2023-05-11 23:19:32 +0300

    Adăugare exemplu
    * Am adăugat exemple de formatare a textului în HTML

---
+++ Pages/Exemple/Formatare.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebDesignRazor.Pages.Exemple
+{
+    public class FormatareModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
